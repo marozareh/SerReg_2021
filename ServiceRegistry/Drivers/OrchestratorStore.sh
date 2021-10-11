b7b171853adf18bdb67eff45ce14701b900aca41
marozareh <marozareh@yahoo.com>
2021-10-11 13:36:56 +0200

    fixes

--- ServiceRegistry/Drivers/OrchestratorStore.sh
+++ ServiceRegistry/Drivers/OrchestratorStore.sh
@@ -1,2 +1,2 @@
 dot -T png /home/jenkins/graphwalker/ServiceRegistry/Drivers/OrchestratorStore.dot > /home/jenkins/graphwalker/ServiceRegistry/Drivers/OrchestratorStore.png
-cp -rf /home/jenkins/graphwalker/ServiceRegistry/Drivers/OrchestratorStore.png /home/jenkins/workspace/OrchestratorStore/OrchestratorStore.png
+cp -rf /home/jenkins/graphwalker/ServiceRegistry/Drivers/OrchestratorStore.png /home/jenkins/workspace/OrchestrationStore/OrchestratorStore.png
